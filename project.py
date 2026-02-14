0b16096d4f2ce435b49ca27c7fc81fd58e1616ea
igorignjatoviic <ignjatovici2006@gmail.com>
2026-02-14 14:07:05 +0100

    Project

--- project.py
+++ project.py
@@ -1,7 +1,34 @@
 import random, math
 
-# selection
-def generateInitialPopulation(citiesSize, populationSize):
+def generateGenerations(numGenerations, cities):    # generates generations and chooses the best solution for each
+    population = generateInitialPopulation(len(cities))
+    for i in range(numGenerations):
+        population = generateNewPopulation(cities, population)
+
+        distances = [totalDistance(route, cities) for route in population]
+        bestDistance = min(distances)
+        bestRoute = population[distances.index(bestDistance)]
+
+        print(f"Generation {i + 1}: ", end="")
+        for city in bestRoute:
+            print(f"{city} -> ", end="")
+        print(bestRoute[0])
+        print()
+        
+def generateNewPopulation(cities, population):      # generates new population based on the old one
+    newPopulation = []
+    newPopulation.append(elitis(cities, population))
+
+    while len(newPopulation) < len(population):
+        stParent = tournamentSelection(cities, population, chromosomes=10)
+        ndParent = tournamentSelection(cities, population, chromosomes=10)
+        child = orderCrossover(stParent, ndParent)
+        child = swapMutation(child)
+        newPopulation.append(child)
+    
+    return newPopulation
+
+def generateInitialPopulation(citiesSize, populationSize=100):      # generates first population
     population = []
     for _ in range(populationSize):
         route = list(range(1, citiesSize+1))
@@ -10,9 +37,8 @@ def generateInitialPopulation(citiesSize, populationSize):
 
     return population
 
-def totalDistance(route, cities):
+def totalDistance(route, cities):       # calculates Euclidean distance
     distance = 0
-    print(route)
     for i in range(len(route)):
         stCity = cities[route[i]]
         ndCity = cities[route[(i+1) % len(route)]]
@@ -20,7 +46,38 @@ def totalDistance(route, cities):
     
     return distance
 
-def loadCities(filename):
+def tournamentSelection(cities, population, chromosomes=3):     # tournament selection method
+    selected = random.sample(population, chromosomes)
+    best = min(selected, key=lambda route: totalDistance(route, cities))
+    return best
+
+def orderCrossover(stParent, ndParent):     # crossover method that takes interval of first parent, second parent is filling him up
+    child = [None] * len(stParent)
+    start, end = sorted(random.sample(range(1, len(stParent)+1), 2))
+    child[start:end] = stParent[start:end]
+    
+    ndParentIdx = 0
+    for i in range(len(child)):
+        if child[i] is None:
+            while ndParent[ndParentIdx] in child:
+                ndParentIdx += 1
+            child[i] = ndParent[ndParentIdx]
+    
+    return child
+
+def swapMutation(route, mutationRate=0.2):      # swap mutation method, permutation of list if conditions are met
+    newRoute = route.copy()
+    if random.random() < mutationRate:
+        i, j = random.sample(range(len(newRoute)), 2)
+        newRoute[i], newRoute[j] = newRoute[j], newRoute[i]
+
+    return newRoute
+
+def elitis(cities, population):     # returns first best route
+    best = min(population, key=lambda route: totalDistance(route, cities))
+    return best
+
+def loadCities(filename):   # loads cities from .txt into dict
     cities = {}
     with open(filename, 'r') as f:
         lines = f.readlines()
@@ -32,6 +89,4 @@ def loadCities(filename):
 
 if __name__ == "__main__":
     cities = loadCities("resources/data_tsp.txt")
-    population = generateInitialPopulation(len(cities), 100)
-    for route in population:
-        print(totalDistance(route, cities))
+    generateGenerations(5, cities)
